2c93b7ba3ee74cc2d8694af515c670d50ffd3bd4
Zenovsky <dudyakhlebniy@gmail.com>
2025-09-13 12:45:11 +0300

    fix square.py - убрал self.side_a = side_a

--- src/square.py
+++ src/square.py
@@ -6,7 +6,5 @@ class Square(Rectangle):
     def __init__(self, side_a: int):
         if side_a <= 0:
             raise ValueError(f'square should have side great than 0, actual {side_a}')
-        self.side_a = side_a
 
         super().__init__(side_a, side_a)
-        self.side_a = side_a
